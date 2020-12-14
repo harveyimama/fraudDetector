a15837b5a955ed946cea8ceb362edc9294522e66
harveyimama <harveyimama@yahoo.com>
2020-12-14 01:07:33 +0100

    Tree based model test done

--- Analysis/nycFinalProject.R
+++ Analysis/nycFinalProject.R
@@ -38,7 +38,7 @@ table(truth = trans[train1,]$V16, prediction = LC.predicted)
 
 test_dats =trans[test,] %>% filter(trans[test,]$V5 != 'WEBSERVICE' & trans[test,]$V6 != 'POS-TRANSFER')
 
-logit.test <- ifelse((predict.glm(logit.overall4,test_dats,type="response"))>0.2, "TRUE", "FALSE")
+logit.test <- ifelse((predict.glm(logit.overall4,test_dats,type="response"))>0.5, "TRUE", "FALSE")
 confusion = table(truth = test_dats$V16, prediction = logit.test)
 acc = (confusion[1,1]+confusion[2,2])/nrow(test_dats)
 
@@ -80,7 +80,40 @@ acc = (confusion[1,1]+confusion[2,2])/nrow(OJ.test)
 #####################
 
 library(tree)
-initial.tree = tree(V16 ~ .+V2, split = "gini", data =  trans[train1,])
+trans$V3 = as.factor(trans$V3 )
+trans$V4 = as.factor(trans$V4 )
+trans$V5 = as.factor(trans$V5 )
+trans$V6 = as.factor(trans$V6 )
+trans$V10 = as.factor(trans$V10 )
+trans$V11 = as.factor(trans$V11 )
+trans$V12 = as.factor(trans$V12 )
+trans$V13 = as.factor(trans$V13 )
+trans$V14 = as.factor(trans$V14 )
+trans$V15 = as.factor(trans$V15 )
+trans$V18 = as.factor(trans$V18 )
+trans$V16 = as.factor(trans$V16 )
+summary(trans)
+train1 = sample(1:nrow(trans), 7*nrow(trans)/10)
+test = (-train1)
+initial.tree = tree(V16 ~ .-V18-V6-V1-V9, split = "gini", data =  trans[train1,],
+                    control=tree.control(nrow(trans[train1,]), mincut = 10000))
+summary(initial.tree)
 
+tree.predictions = predict(initial.tree, trans[test,], type = "class")
+tree.likelihood = predict(initial.tree, trans[test,])
+confusion = table(tree.predictions, trans[test,]$V16)
+acc = (confusion[1,1]+confusion[2,2])/nrow(test_dats)
+
+tree.predictions
+
+
+cv.training = cv.tree(initial.tree, FUN = prune.misclass)
+best.nodes = cv.training$size[which(cv.training$dev == min(cv.training$dev))] # optimal number of nodes is 11
+prune.tree = prune.misclass(initial.tree, best = best.nodes)
+
+summary(prune.tree) 
+tree.predictions = predict(prune.tree, trans[test,], type = "class")
+confusion = table(tree.predictions, trans[test,]$V16)
+acc = (confusion[1,1]+confusion[2,2])/nrow(test_dats)
 
 
